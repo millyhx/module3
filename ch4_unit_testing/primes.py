3a42f2106985248e540f9f38361e312f702cfe48
Milly Huckle <millycyb@outlook.com>
2019-01-31 11:22:32 +0000

    updating chapter 3 and 4 files

--- ch4_unit_testing/primes.py
+++ ch4_unit_testing/primes.py
@@ -1,11 +1,19 @@
-
-
-#Will return True if number is prime.
 def is_prime(number):
-    for element in range(number):
+    if number <= 1:
+        return False
+    for element in range(2, number):
         if number % element == 0:
             return False
     return True
+
+def print_next_prime(number):
+    index = number
+    while True:
+        index += 1
+        if is_prime(index):
+            print(index)
+
+
 
 
 
--- ch4_unit_testing/test_primes.py
+++ ch4_unit_testing/test_primes.py
@@ -1,12 +1,19 @@
-
 import unittest
 from primes import is_prime
 
-class PrimesTest(unittest.TestCase):
-    def test_prime(self):
-        self.assertTrue(is_prime(5.3))
+class PrimesTestCase(unittest.TestCase):
+    def test_is_five_prime(self):
+        self.assertTrue(is_prime(5))
+
+    def test_is_four_not_prime(self):
+        self.assertTrue(is_prime(4), msg="Four is not prime!")
+        
+    def test_is_zero_not_prime(self):
+        self.assertFalse(is_prime(0))
+    
+    def test_negative_number(self):
+        self.assertFalse(is_prime(index))
         
 if __name__ == "__main__":
     unittest.main()
-
 
